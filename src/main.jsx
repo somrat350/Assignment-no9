5f60784b182b0cca0329c7ea67b3364333c54e07
somrat350 <mdsomratsordaro350@gmail.com>
2025-10-30 10:59:28 +0600

    solve some error

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,11 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router'
-import { router } from './Routers/Router'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
-=======
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
-import { router } from "./Router/Router";
-import AuthProvider from "./Context/AuthContext/AuthProvider";
+import { router } from "./Routers/Router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <RouterProvider router={router} />
   </StrictMode>
 );
->>>>>>> 2a673b2 (completed full project)
